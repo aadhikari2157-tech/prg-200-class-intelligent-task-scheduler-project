137468b1cb2e891cf0094e1ebe9c113af27db9bb
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-06-08 21:20:01 +0545

    Update scheduler.py

--- task_scheduler/scheduler.py
+++ task_scheduler/scheduler.py
@@ -1,1 +1,8 @@
 from datetime import datetime
+def calculate_duration_minutes(start_time, end_time):
+    try:
+        sh, sm = map(int, start_time.split(':'))
+        eh, em = map(int, end_time.split(':'))
+        return (eh * 60 + em) - (sh * 60 + sm)
+    except:
+        return 60
