6b997d137e662df85a9d763be81543d22886648d
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-06-09 19:30:16 +0545

    Update app.py

--- task_scheduler/app.py
+++ task_scheduler/app.py
@@ -3,3 +3,9 @@ from flask_login import LoginManager
 from models import db, User
 from routes import main, auth
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'taskassigner-secret-2025'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
